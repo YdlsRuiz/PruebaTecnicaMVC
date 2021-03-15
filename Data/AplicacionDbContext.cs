b9ad16dfec00c9a0d1e111e3fee7394b94097345
Yalin Ruiz <yalin19992006@gmail.com>
2021-03-15 02:19:42 -0400

    DB CONTEXT CREADO y actualiaciones tecnicas

---
+++ Data/AplicacionDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PruebaTecnicaMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PruebaTecnicaMVC.Data
+{
+    public class AplicacionDbContext: DbContext
+    {
+        public AplicacionDbContext(DbContextOptions<AplicacionDbContext>options): base(options)
+        {
+
+        }
+
+        public DbSet<Persona> Persona { get; set; }
+    }
+}
